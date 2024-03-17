4ae0b97a6bf98265233c958e1b89e8f282d040de
Luong Dang <dangvluong@gmail.com>
2024-03-17 23:06:04 +0700

    Add backend code

---
+++ movies-backend/src/main/java/com/project/movies/repositories/ReviewRepository.java
@@ -0,0 +1,11 @@
+package com.project.movies.repositories;
+
+import com.project.movies.domains.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+
+}
